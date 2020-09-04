294c3d769ea894c908d1c38b2e7f2bb4eb0dfe4a
Christoph Werner <christoph+github@codepunkt.de>
2020-09-04 11:43:00 +0200

    feat: v1.1.0 - restrict styled-components import to /macro

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
-const eslintConfigReactApp = require("eslint-config-react-app");
+const eslintConfigReactApp = require('eslint-config-react-app')
 
 // remove flowtype plugin and set ecmaVersion to 2020
 module.exports = {
   ...eslintConfigReactApp,
-  plugins: eslintConfigReactApp.plugins.filter((p) => p !== "flowtype"),
+  plugins: eslintConfigReactApp.plugins.filter((p) => p !== 'flowtype'),
   parserOptions: {
     ...eslintConfigReactApp.parserOptions,
     ecmaVersion: 2020,
@@ -20,10 +20,21 @@ module.exports = {
     },
   ],
   // remove all keys that start with "flowtype/"
-  rules: Object.entries(eslintConfigReactApp.rules).reduce(
-    (acc, [key, val]) => {
-      return key.startsWith("flowtype/") ? acc : { ...acc, [key]: val };
-    },
-    {}
-  ),
-};
+  rules: {
+    ...Object.entries(eslintConfigReactApp.rules).reduce((acc, [key, val]) => {
+      return key.startsWith('flowtype/') ? acc : { ...acc, [key]: val }
+    }, {}),
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'styled-components',
+            message: 'Please import from styled-components/macro',
+          },
+        ],
+        patterns: ['!styled-components/macro'],
+      },
+    ],
+  },
+}
